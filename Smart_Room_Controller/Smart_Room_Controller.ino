b9dd4f30f367abf57f9094afaa60918d9217eb6b
Christian Chavez <schavezent@gmail.com>
2020-10-27 11:49:52 -0600

    Light control code done

--- Smart_Room_Controller/Smart_Room_Controller.ino
+++ Smart_Room_Controller/Smart_Room_Controller.ino
@@ -12,13 +12,19 @@
 #include <Wire.h> //not sure what this does
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <mac.h>
+#include <wemo.h> //in lesson code this was in parenthesis
+#include <Encoder.h>
+#include <hue.h>
+#include <Ethernet.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64 //changed for higher resolution
 #define OLED_RESET 4
 
 int envRead = 0;
-int sceneControl = 1;
+int lightControl = 1;
 int homeControl = 2;
 int autoWind = 3;
 
@@ -26,14 +32,14 @@ int encButton = 23;
 int blackButton = 22;
 int textSize = 1;
 
-OneButton button1(blackButton, false, false); //may need to change false true since button code is borrowed from encoder lesson
-Adafruit_BME280 bme;
-Adafruit_NeoPixel pixel(12, 17, NEO_GRB + NEO_KHZ800);
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-
-
-
+bool HueOn;
+int HueColor;
+int HueBright = 255; //declared immediatly to set initial brightness to 255
+int brightPos; //mapped to encPos to control brightness of hue
+int encPos; //posistion of encoder
+int lightNum [4]; //array for choosing hue light
+int rainColor; //chooses color of rainbow from hue color array
+//maybe modify hue.h to add white to rainbow array*******
 
 int homeState; //will cycle between functions of controller
 
@@ -43,11 +49,30 @@ float humidity;
 float tempRange;
 int neoRange;
 
+bool buttonState;
+
+OneButton button1(blackButton, false, false); //may need to change false true since button code is borrowed from encoder lesson
+OneButton button2(encButton, false, false);
+Adafruit_BME280 bme;
+Adafruit_NeoPixel pixel(12, 17, NEO_GRB + NEO_KHZ800);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Encoder myEnc(3,2);
+
+EthernetClient client;
+
 void setup() {
+
+Ethernet.begin(mac);
 
 button1.attachClick(click1); //black button
 button1.setClickTicks(250);
 button1.setPressTicks(2000);
+
+button2.attachClick(click2); //encoder button
+button2.setClickTicks(250);
+button2.setPressTicks(2000);
+
+pinMode (17, OUTPUT); //is this needed?
 
 pixel.begin();
 pixel.show();
@@ -61,9 +86,12 @@ display.display();
 
 }
 
-void loop() {
+void loop() { 
 
-  if(homeState == envRead) {
+  button1.tick();
+  button2.tick();
+
+  if(homeState == envRead) { //could add functionality to turn lights on and off as well as read environment 
     temp = ((bme.readTemperature()*9/5)+32); //reads temp and converts to Fehrenheit
     pressure = ((bme.readPressure()/100.0)/3386); //reads pressure and converts from pascals to inHG
     humidity = bme.readHumidity(); //reads humidity percentage
@@ -84,6 +112,38 @@ void loop() {
     display.display();
   }
 
+  if(homeState == lightControl) {
+
+    encPos = myEnc.read();
+    if(encPos > 95){
+      encPos = 95;
+    }
+    if(encPos < 0){
+      encPos = 0;
+    }
+    
+    myEnc.write(encPos);
+    brightPos = map(encPos, 0, 95, 0, 255);
+    
+    HueOn = buttonState;
+    HueColor = HueRainbow[rainColor];
+    HueBright = brightPos;
+    setHue(lightNum[4], HueOn, HueColor, HueBright); //may need to adjust lightNum array or add several setHue
+
+    if(buttonState == true){ //neoPixel will show same color as hue bulb without delay
+      pixel.clear();
+      pixel.setBrightness(brightPos);//pixel will possibly show same brightness as hue bulb***
+      pixel.fill(rainbow[rainColor], 0, 12);
+      pixel.show();
+      }
+      else {
+      pixel.clear();
+      pixel.fill(rainbow[rainColor], 0, 1);
+      pixel.show();
+      }
+        
+  }
+
 }
 
 void click1() { //black button press will cycle between functions
@@ -94,3 +154,24 @@ void click1() { //black button press will cycle between functions
     homeState++;
   }
 }
+
+void click2() { //encoder button will cycle button state
+  if(buttonState == true) { 
+    buttonState = false;
+  }
+  else {
+    buttonState = true;
+  } //maybe this button state can turn on or off extra LEDs
+}
+
+void longPressStart2() { //encoder long press will cycle bulb through colors. 
+   if(rainColor > 6){
+     rainColor = 0;
+   }
+   else{
+      rainColor++;
+  }
+}
+
+
+//could easily implement long press 1 to cycle language on display 
