96266b0c9d13db66f9a8272d191e6bd71b71b3d6
Christian Chavez <schavezent@gmail.com>
2020-10-27 09:52:30 -0600

    Basic code functionality in place

--- Smart_Room_Controller/Smart_Room_Controller.ino
+++ Smart_Room_Controller/Smart_Room_Controller.ino
@@ -5,12 +5,41 @@
  * Date:        10-27-2020
  */
 
+#include <OneButton.h>
+
+int envRead = 0;
+int sceneControl = 1;
+int homeControl = 2;
+int autoWind = 3;
+
+int encButton = 23;
+int blackButton = 22;
+
+OneButton button1(blackButton, false, false); //may need to change false true since button code is borrowed from encoder lesson
+
+int homeState; //will cycle between functions of controller
+
 void setup() {
-  // put your setup code here, to run once:
+
+button1.attachClick(click1); //black button
+button1.setClickTicks(250);
+button1.setPressTicks(2000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if(homeState == envRead) {
+    
+  }
+
+}
+
+void click1() { //black button press will cycle between functions
+  if(homeState > 3){
+    homeState = 0;
+  }
+  else {
+    homeState++;
+  }
 }
