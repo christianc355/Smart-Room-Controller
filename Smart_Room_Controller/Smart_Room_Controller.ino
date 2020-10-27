493b43ec7f54c98f59a965ed3db58ac6f90fd005
Christian Chavez <schavezent@gmail.com>
2020-10-27 09:03:38 -0600

    Morning commit

---
+++ Smart_Room_Controller/Smart_Room_Controller.ino
@@ -0,0 +1,16 @@
+/*
+ * Project:     Smart Room Controller
+ * Description: A Home Controller Designed for the Hard of Hearing
+ * Author:      Christian Chavez
+ * Date:        10-27-2020
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
