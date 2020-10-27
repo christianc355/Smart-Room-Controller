a547ae10faccbddd415780b117ae758f68cfbd3d
Christian Chavez <schavezent@gmail.com>
2020-10-27 15:47:11 -0600

    Home control complete

--- Smart_Room_Controller/Smart_Room_Controller.ino
+++ Smart_Room_Controller/Smart_Room_Controller.ino
@@ -14,10 +14,10 @@
 #include <Adafruit_SSD1306.h>
 #include <SPI.h>
 #include <mac.h>
-#include <wemo.h> //in lesson code this was in parenthesis
+#include "wemo.h" //in lesson code this was in parenthesis
 #include <Encoder.h>
 #include <hue.h>
-#include <Ethernet.h>
+//#include <Ethernet.h> //included already in wemo.h
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64 //changed for higher resolution
@@ -28,6 +28,8 @@ int lightControl = 1;
 int homeControl = 2;
 int autoWind = 3;
 
+int wemoOutlet[3]; //array to select wemo outlet
+
 int encButton = 23;
 int blackButton = 22;
 int textSize = 1;
@@ -37,8 +39,10 @@ int HueColor;
 int HueBright = 255; //declared immediatly to set initial brightness to 255
 int brightPos; //mapped to encPos to control brightness of hue
 int encPos; //posistion of encoder
+int wemoPos; //posistion of Wemo switch mapped to encoder
 int lightNum [4]; //array for choosing hue light
 int rainColor; //chooses color of rainbow from hue color array
+
 //maybe modify hue.h to add white to rainbow array*******
 
 int homeState; //will cycle between functions of controller
@@ -49,14 +53,26 @@ float humidity;
 float tempRange;
 int neoRange;
 
-bool buttonState;
+const int alien = 0; //numbers relate to wemo outlets
+const int whiteFan = 1;
+const int tea = 2;
+const int blueFan = 3;
 
-OneButton button1(blackButton, false, false); //may need to change false true since button code is borrowed from encoder lesson
+bool buttonState; 
+
+bool alienState; //will switch states of different wemo outlets between true and false
+bool whiteFanState;
+bool teaState;
+bool blueFanState;
+
+OneButton button1(blackButton, false, false); //may need to change false true since button code is borrowed from encoder lesson****
 OneButton button2(encButton, false, false);
 Adafruit_BME280 bme;
 Adafruit_NeoPixel pixel(12, 17, NEO_GRB + NEO_KHZ800);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Encoder myEnc(3,2);
+
+wemo wemoClass;
 
 EthernetClient client;
 
@@ -90,8 +106,24 @@ void loop() {
 
   button1.tick();
   button2.tick();
+    
+  if(homeState == envRead) {
+     readEnvironment();
+    }
 
-  if(homeState == envRead) { //could add functionality to turn lights on and off as well as read environment 
+  if(homeState == lightControl) {
+    controlLights();
+   }
+  
+  if(homeState == homeControl){
+    controlHome();
+  }
+  
+}
+
+
+void readEnvironment(){      //could add functionality to turn lights on and off as well as read environment 
+
     temp = ((bme.readTemperature()*9/5)+32); //reads temp and converts to Fehrenheit
     pressure = ((bme.readPressure()/100.0)/3386); //reads pressure and converts from pascals to inHG
     humidity = bme.readHumidity(); //reads humidity percentage
@@ -110,11 +142,11 @@ void loop() {
     display.setCursor(0,0);
     display.printf("Temperature: %.2f %cF\n Pressure: %2f inHG\n Humidity: %.2f%c\n", temp, (char)247, pressure, humidity, (char)37);
     display.display();
-  }
+}
 
-  if(homeState == lightControl) {
-
-    encPos = myEnc.read();
+void controlLights(){
+  
+  encPos = myEnc.read();
     if(encPos > 95){
       encPos = 95;
     }
@@ -141,11 +173,63 @@ void loop() {
       pixel.fill(rainbow[rainColor], 0, 1);
       pixel.show();
       } 
+
+    display.clearDisplay();
+    display.setTextSize(textSize); //maybe increase text size****
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.printf("Light Control");
+    display.display();
+}
+
+void controlHome(){
+  encPos = myEnc.read();
+  if(encPos > 95){
+    encPos = 95;
+  }
+  if(encPos < 0){
+    encPos = 0;
   }
 
-  if(homeState == homeControl
+  myEnc.write(encPos);
+  wemoPos = map(encPos, 0, 95, 0, 3);
 
+  if(wemoPos == 0){
+    if(alienState == true){
+      wemoClass.switchON(alien);
+    }
+    else{
+      wemoClass.switchOFF(alien);
+    }
+  }
+  if(wemoPos == 1){
+    if(whiteFanState == true){
+      wemoClass.switchON(whiteFan);
+    }
+    else{
+      wemoClass.switchOFF(whiteFan);
+    }
+  }
+  if(wemoPos == 2){
+    if(alienState == true){
+      wemoClass.switchON(tea);
+    }
+    else{
+      wemoClass.switchOFF(tea);
+    }
+  }
+  if(wemoPos == 3){
+    if(alienState == true){
+      wemoClass.switchON(blueFan);
+    }
+    else{
+      wemoClass.switchOFF(blueFan);
+    }
+  }
 }
+
+
+
 
 void click1() { //black button press will cycle between functions
   if(homeState > 3){
@@ -156,23 +240,65 @@ void click1() { //black button press will cycle between functions
   }
 }
 
-//maybe add different fuctionality to click to depending on home state. if home state ==1 click does this 
-void click2() { //encoder button will cycle button state
-  if(buttonState == true) { 
-    buttonState = false;
+ 
+void click2() { //encoder button will cycle different states depeding on homeState
+  if(homeState == lightControl) { //controls what encoder button will do if within lightControl funcionality
+    if(buttonState == true) { 
+      buttonState = false;
+    }
+    else {
+      buttonState = true;
+    } //maybe this button state can turn on or off extra LEDs
   }
-  else {
-    buttonState = true;
-  } //maybe this button state can turn on or off extra LEDs
-}
 
-void longPressStart2() { //encoder long press will cycle bulb through colors. 
-   if(rainColor > 6){
-     rainColor = 0;
-   }
-   else{
-      rainColor++;
+  if(homeState == homeControl){ //controls what button will do within homeControl
+    if(wemoPos == 0){
+      if(alienState == true) { 
+         alienState = false;
+      }
+      else {
+        alienState = true;
+      }
+    }
+     
+    if(wemoPos == 1){    
+     if(whiteFanState == true) { 
+        whiteFanState = false;
+     }
+     else {
+        whiteFanState = true;
+     }
+    }
+    
+    if(wemoPos == 2){     
+     if(teaState == true) { 
+       teaState = false;
+    }
+      else {
+        teaState = true;
+     }
+    }
+    
+    if(wemoPos == 3){
+     if(blueFanState == true) { 
+       blueFanState = false;
+    }
+     else {
+       blueFanState = true;
+     }
+    } 
   }
+}
+  
+void longPressStart2() { //encoder long press will cycle bulb through colors. 
+
+   if(homeState = lightControl);
+     if(rainColor > 6){
+       rainColor = 0;
+     }
+     else{
+        rainColor++;
+    }
 }
 
 
